8123c332bf68da6e2070d774e8816adf46dc1c14
Mário Menezes <mario082@gmail.com>
2021-02-11 09:01:12 -0300

    posicionando spinner

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,13 +13,13 @@ const Footer = lazy(() => import('../Footer'));
 function Home() {
   return (
     <>
-      <Suspense fallback={<p style={{textAlign: "center", verticalAlign: "center", height: "300px"}}>Loading Banner...</p>}>
+      <Suspense fallback={<img style={{height: 300, position: "absolute", top: 0, bottom: 0, left: 0, right: 0, margin: "auto"}} src="https://www.eurobitume.eu/fileadmin/generic/pits_downloadcenter/Resources/Public/images/loading.gif" alt="loading..."/> }>
         <Banner />
       </Suspense>
-      <Suspense fallback={<p style={{textAlign: "center", verticalAlign: "center", height: "300px"}}>Loading Posts...</p>}>
+      <Suspense fallback={<p style={{top: 300, bottom: 0, left: 0, right: 0, margin: "auto"}} src="https://www.eurobitume.eu/fileadmin/generic/pits_downloadcenter/Resources/Public/images/loading.gif" alt="loading..."/> }>
         <Cards />
       </Suspense>
-      <Suspense fallback={<p style={{textAlign: "center", verticalAlign: "center", height: "300px"}}>Loading Banner...</p>}>
+      <Suspense fallback={<p style={{top: 500, bottom: 0, left: 0, right: 0, margin: "auto"}} src="https://www.eurobitume.eu/fileadmin/generic/pits_downloadcenter/Resources/Public/images/loading.gif" alt="loading..."/> }>
       <Footer />
       </Suspense>
 
