883471eb94f05d072c74b86fb32ff3164ea8ddaf
Mário Menezes <mario082@gmail.com>
2021-02-11 00:24:34 -0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,55 +8,47 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Receba no seu e-mail, dicas e promoções em primeira mão.
         </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
+        
         <div className='input-areas'>
           <form>
             <input
               className='footer-input'
               name='email'
               type='email'
-              placeholder='Your Email'
+              placeholder='Seu e-mail'
             />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
+            <Button buttonStyle='btn--outline'>Inscreva-se</Button>
           </form>
         </div>
       </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+            <h2>reCoffee</h2>
+            <Link to='/'>Quem somos</Link>
+            <Link to='/'>Depoimentos</Link>
+            <Link to='/'>Termos de Serviço</Link>
           </div>
           <div class='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <h2>Fale conosco</h2>
+            <Link to='/'>Contato</Link>
+            <Link to='/'>Dúvidas</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <h2>Vídeos</h2>
+            <Link to='/'>Receitas</Link>
+            <Link to='/'>Curiosidades</Link>
+            <Link to='/'>Métodos</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
             <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
           </div>
         </div>
       </div>
@@ -64,11 +56,11 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
-              <i class='fab fa-typo3' />
+              reCoffe
+              <i style={{margin: 10}} class='fas fa-mug-hot' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>reCoffee © 2020</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             reCoffee  
-            <i class='fas fa-mug-hot' />
+            <i style={{margin: 10}} class='fas fa-mug-hot' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
